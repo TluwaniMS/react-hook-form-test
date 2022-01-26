895092bc8015c52e81537c8851229b469d7791c5
tluwaniMS <tluwani2020@gmail.com>
2022-01-26 16:08:54 +0200

    adding-doctor-creation-form-with-styling

--- src/components/doctor-creation-form/DoctorCreationForm.js
+++ src/components/doctor-creation-form/DoctorCreationForm.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ResponseHandler from "../response-handler/ResponseHandler";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUserMd, faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { faUserMd, faArrowLeft, faUndoAlt } from "@fortawesome/free-solid-svg-icons";
 import { specialties } from "../../display-support/specialties-support";
 import { genders } from "../../display-support/gender-support";
 import "./DoctorCreationForm.css";
@@ -33,7 +33,43 @@ const DoctorCreationForm = () => {
         <FontAwesomeIcon icon={faUserMd} />
       </div>
       <div className="doctors-view-text">Add a doctor to the directrory</div>
-      <div className="doctor-creation-form-container"></div>
+      <div className="doctor-creation-form-container ">
+        <form className="form-input-container">
+          <div className="input-container">
+            <input placeholder="name" className="input"></input>
+          </div>
+          <div className="input-container">
+            <input placeholder="surname" className="input"></input>
+          </div>
+          <div className="input-container">
+            <input placeholder="email" className="input"></input>
+          </div>
+          <div className="input-container">
+            <select>
+              {specialties.map((specialty) => (
+                <option key={specialty.label} value={specialty.value}>
+                  {specialty.label}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="input-container">
+            <select>
+              {genders.map((gender) => (
+                <option key={gender.label} value={gender.value}>
+                  {gender.label}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="doctors-creation-button-container">
+            <button>submit</button>
+          </div>
+          <div className="form-refresh-container">
+            <FontAwesomeIcon icon={faUndoAlt} />
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
