2a2aab094691d5f95214d4d927c14f03d3172c3d
tluwaniMS <tluwani2020@gmail.com>
2022-01-26 13:26:00 +0200

    adding-initial-view-elements-on-doctors-view

--- src/components/doctor-creation-form/DoctorCreationForm.js
+++ src/components/doctor-creation-form/DoctorCreationForm.js
@@ -1,8 +1,19 @@
 import React from "react";
 import ResponseHandler from "../response-handler/ResponseHandler";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUserMd } from "@fortawesome/free-solid-svg-icons";
+import "./DoctorCreationForm.css";
 
 const DoctorCreationForm = () => {
-  return <div>Doctors creation form works!!!</div>;
+  return (
+    <div className="main-doctor-creation-form-container">
+      <div className="doctors-view-icon">
+        <FontAwesomeIcon icon={faUserMd} />
+      </div>
+      <div className="doctors-view-text">Add a doctor to the directrory</div>
+      <div className="doctor-creation-form-container"></div>
+    </div>
+  );
 };
 
 export default DoctorCreationForm;
