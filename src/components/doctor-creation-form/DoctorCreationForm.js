25ba2f1fa958de436acd63923611fc64647939ce
tluwaniMS <tluwani2020@gmail.com>
2022-01-26 14:15:40 +0200

    adding-home-button-navigation

--- src/components/doctor-creation-form/DoctorCreationForm.js
+++ src/components/doctor-creation-form/DoctorCreationForm.js
@@ -1,14 +1,34 @@
 import React from "react";
 import ResponseHandler from "../response-handler/ResponseHandler";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUserMd } from "@fortawesome/free-solid-svg-icons";
+import { faUserMd, faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 import { specialties } from "../../display-support/specialties-support";
 import { genders } from "../../display-support/gender-support";
 import "./DoctorCreationForm.css";
+import { useNavigate } from "react-router-dom";
 
 const DoctorCreationForm = () => {
+  const navigate = useNavigate();
+
+  const navigateHome = () => {
+    navigate("/");
+  };
+
   return (
     <div className="main-doctor-creation-form-container">
+      <div className="back-button-container">
+        <button
+          className="home-navigation-button"
+          onClick={() => {
+            navigateHome();
+          }}
+        >
+          <span className="back-icon-container">
+            <FontAwesomeIcon icon={faArrowLeft} />
+          </span>
+          Home
+        </button>
+      </div>
       <div className="doctors-view-icon">
         <FontAwesomeIcon icon={faUserMd} />
       </div>
