321ed12501abf2ef2d91657218e40556d15454d5
tluwaniMS <tluwani2020@gmail.com>
2022-01-26 11:46:06 +0200

    adding-buttons-for-creation-navigation

--- src/components/home-view/HomeView.js
+++ src/components/home-view/HomeView.js
@@ -1,7 +1,20 @@
 import React from "react";
+import "./HomeView.css";
+import { Link } from "react-router-dom";
 
 const HomeView = () => {
-  return <div>Home view works!!</div>;
+  return (
+    <div className="main-content-cointainer">
+      <div className="links-container">
+        <Link to={`/login`} className="login-link">
+          Login
+        </Link>
+        <Link to={`/doctor-creation-form`} className="doctor-creation-link">
+          Doctor
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default HomeView;
--- src/components/response-handler/ResponseHandler.js
+++ src/components/response-handler/ResponseHandler.js
@@ -1,4 +1,6 @@
 import React from "react";
+import ErrorDisplay from "../error-display/ErrorDisplay";
+import SuccessDisplay from "../success-display/SuccessDisplay";
 
 const ResponseHandler = ({ operation, data }) => {
   return <div>Log form works!!!</div>;
