67f33f358c100da9f53f35a8697bd51d177533ad
tluwaniMS <tluwani2020@gmail.com>
2022-01-26 20:40:28 +0200

    adding-login-forms-with-out-validation-logic

--- src/components/login-form/LoginForm.js
+++ src/components/login-form/LoginForm.js
@@ -3,7 +3,7 @@ import "./LoginForm.css";
 import ResponseHandler from "../response-handler/ResponseHandler";
 import { useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowLeft, faSignInAlt } from "@fortawesome/free-solid-svg-icons";
+import { faArrowLeft, faSignInAlt, faUndoAlt } from "@fortawesome/free-solid-svg-icons";
 
 const LoginForm = () => {
   const navigate = useNavigate();
@@ -31,6 +31,23 @@ const LoginForm = () => {
 
       <div className="login-view-icon">
         <FontAwesomeIcon icon={faSignInAlt} />
+      </div>
+
+      <div className="login-form-container">
+        <form className="form-input-container">
+          <div className="input-container">
+            <input placeholder="email" className="input"></input>
+          </div>
+          <div className="input-container">
+            <input placeholder="password" type="password" className="input"></input>
+          </div>
+          <div className="login-button-container">
+            <button type="submit">submit</button>
+          </div>
+          <div className="form-refresh-container">
+            <FontAwesomeIcon icon={faUndoAlt} />
+          </div>
+        </form>
       </div>
     </div>
   );
