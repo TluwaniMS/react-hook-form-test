3755e1a91cf0f07a743715f97c57479685f46d30
tluwaniMS <tluwani2020@gmail.com>
2022-01-26 20:13:43 +0200

    adding-back-button-and-main-view-icon-to-login-display

--- src/components/login-form/LoginForm.js
+++ src/components/login-form/LoginForm.js
@@ -1,8 +1,39 @@
 import React from "react";
+import "./LoginForm.css";
 import ResponseHandler from "../response-handler/ResponseHandler";
+import { useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft, faSignInAlt } from "@fortawesome/free-solid-svg-icons";
 
 const LoginForm = () => {
-  return <div>Log form works!!!</div>;
+  const navigate = useNavigate();
+
+  const navigateHome = () => {
+    navigate("/");
+  };
+  return (
+    <div className="main-login-form-container">
+      <div>
+        <div className="back-button-container">
+          <button
+            className="home-navigation-button"
+            onClick={() => {
+              navigateHome();
+            }}
+          >
+            <span className="back-icon-container">
+              <FontAwesomeIcon icon={faArrowLeft} />
+            </span>
+            Home
+          </button>
+        </div>
+      </div>
+
+      <div className="login-view-icon">
+        <FontAwesomeIcon icon={faSignInAlt} />
+      </div>
+    </div>
+  );
 };
 
 export default LoginForm;
